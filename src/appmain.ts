4fd598a4e6f7296e5a807bed842febfd68824bbe
IronVisuals <henriquecesaroni14@gmail.com>
2025-09-16 21:03:42 -0300

    Co-authored-by: JoaoPR578 <JoaoPR578@users.noreply.github.com> Co-authored-by: Cleiton-Muchenski <Cleiton-Muchenski@users.noreply.github.com>

---
+++ src/appmain.ts
@@ -0,0 +1,8 @@
+import * as readlineSync from "readline-sync";
+
+
+console.log("....::::MENU::PIZZIFY::::....");
+console.log("1: Cadastrar");
+console.log("1: Pedido");
+console.log("1: Consultar");
+let dig 
